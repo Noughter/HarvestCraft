d03935f7a7ae0f9eb17a201684f6b65f1f3920c6
Marvin Ponten <mp@albit.de>
2013-06-26 12:15:41 +0200

    general Resource Loading + Config configuaration

---
+++ public_html/js/config.js
@@ -0,0 +1,126 @@
+/* Constants */
+var c = {
+    "DEBUG"                 : false,
+
+    /*
+     * To convert between numeric and named directions:
+     * name2dir : c[name.toUpperCase()]
+     * dir2name : c.DIR_NAMES[dir]
+     */
+
+    // Directions a sprite can face
+    "RESET_DIR"             : -1,
+    "LEFT"                  : 0,
+    "UP"                    : 1,
+    "RIGHT"                 : 2,
+    "DOWN"                  : 3,
+
+    // Available directions, in a *VERY* specific order.
+    "DIR_NAMES"             : [ "left", "up", "right", "down" ],
+
+    // Screen resolution
+    "WIDTH"                 : 800,
+    "HEIGHT"                : 600,
+
+    // States
+    "STATE_INFO"            : me.state.USER + 0,
+    "STATE_INTRO"           : me.state.USER + 1,
+
+    // Keys
+    "KEY_APOS"              : 222, // Apostrophe (aka single-quote)
+
+/*
+ *              COLLISION TRUTH TABLE
+ *          rachel  npc     coin    chest   wall    exit    baddie
+ *  rachel  -       1       2       1       3       4       5
+ *  npc     -       -               1       3               5
+ *  coin    -       -       -       2       3
+ *  chest   -       -       -       -
+ *  wall    -       -       -       -       -
+ *  exit    -       -       -       -       -       -
+ *  baddie  -       -       -       -       -       -       -
+ *
+ * Hyphen means redundant space.
+ * Number specifies collision layers.
+ *
+ * 1 = NO COIN
+ * 2 = NO NPC
+ * 3 = NO CHEST
+ * 4 = EXIT
+ * 5 = LIVING
+ */
+
+    // Chipmunk shape layers
+    "LAYER_NONE"            : 0x00000000,
+    "LAYER_NO_COIN"         : 0x00000001,
+    "LAYER_NO_NPC"          : 0x00000002,
+    "LAYER_NO_CHEST"        : 0x00000004,
+    "LAYER_EXIT"            : 0x00000008,
+    "LAYER_LIVING"          : 0x00000010,
+
+    // INTERACTIVE is a special layer for doing bb queries when `action` is pressed.
+    // Any shapes in this layer will collide! :(
+    "LAYER_INTERACTIVE"     : 0x80000000,
+
+    "LAYER_ALL"             : 0xFFFFFFFF,
+
+    // Chipmunk shape groups
+    "GROUP_COINS"           : 1,
+
+    // Chipmunk collision types
+    "COLLIDE_PLAYER"        : 0x00000001,
+    "COLLIDE_COLLECTIBLE"   : 0x00000002,
+    "COLLIDE_EXIT"          : 0x00000003,
+    "COLLIDE_BADDIE"        : 0x00000004,
+    "COLLIDE_GOODIE"        : 0x00000005
+};
+
+// Helper to enable debug by setting a special hash in the URL.
+if (document.location.hash === "#debug") {
+    c.DEBUG = true;
+}
+
+window.addEventListener("hashchange", function onHashChange(e) {
+    var debug = (document.location.hash === "#debug");
+    me.sys.pauseOnBlur = !debug;
+    cm.setDebug(debug);
+    c.__defineGetter__("DEBUG", function () {
+        return debug;
+    });
+});
+
+// Turn the `c` object into a hash of constants.
+try {
+    Object.keys(c).forEach(function eachKey(key) {
+        if (typeof(c[key]) === "function") {
+            return;
+        }
+
+        c.__defineGetter__(
+            key,
+            (function getterFactory(value) {
+                return function returnValue() {
+                    return value
+                };
+            })(c[key])
+        );
+    });
+}
+catch (e) {
+    // No getters? FAKE CONSTANTS!
+}
+
+
+// Game engine settings.
+me.sys.gravity = 0;
+//me.sys.dirtyRegion = true; // Be fast!
+//me.sys.useNativeAnimFrame = true; // Be faster!
+//cm.setSync(false); // Be fastest!
+//me.debug.renderHitBox = true;
+//me.debug.renderCollisionMap = true;
+me.sys.stopOnAudioError = false;
+
+if (c.DEBUG) {
+    me.sys.pauseOnBlur = false;
+    cm.setDebug(true);
+};
--- public_html/js/game.js
+++ public_html/js/game.js
@@ -1,10 +1,33 @@
+/*
+ * HarvestCraft, a sanboc farming RPG
+ * Copyright (C) 2013  Marvin Ponten
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 
 /* Game namespace */
 var game = {
+    // Whether a dialog box is waiting for input.
+    "modal" : false,
+    // `true` when an object's y-coordinate changes to put it at the proper Z-order.
+    "wantsResort" : false,            
+            
     // Run on page load.
-    "onload": function() {
+    "onload": function() {        
         // Initialize the video.
-        if (!me.video.init("screen", 1280, 768, true, 'auto')) {
+        if (!me.video.init("screen", c.WIDTH, c.HEIGHT)) {
             alert("Your browser does not support HTML5 canvas.");
             return;
         }
@@ -18,16 +41,71 @@ var game = {
 
         // Initialize the audio.
         me.audio.init("mp3,ogg");
+        
+        // Keyboard Bindings
+        me.input.bindKey(me.input.KEY.LEFT, "left");
+        me.input.bindKey(me.input.KEY.RIGHT, "right");
+        me.input.bindKey(me.input.KEY.UP, "up");
+        me.input.bindKey(me.input.KEY.DOWN, "down");
+        me.input.bindKey(me.input.KEY.SPACE, "jump", true);
+
 
         // Set a callback to run when loading is complete.
         me.loader.onload = this.loaded.bind(this);
-
         // Load the resources.
-        me.loader.preload(game.resources);
+        this.loadResources();      
 
         // Initialize melonJS and display a loading screen.
         me.state.change(me.state.LOADING);        
     },
+            
+    "loadResources" : function loadResources() {
+        // Set all resources to be loaded.
+        var resources = [];
+
+        // Graphics.
+        this.resources["img"].forEach(function forEach(value) {
+            resources.push({
+                "name"  : value,
+                "type"  : "image",
+                "src"   : "data/img/" + value + ".png"
+            })           
+        });
+
+        // Maps.
+        this.resources["map"].forEach(function forEach(value) {
+            resources.push({
+                "name"  : value,
+                "type"  : "tmx",
+                "src"   : "data/map/" + value + ".tmx"
+            })
+        });
+
+        // Sound effects.
+        this.resources["sfx"].forEach(function forEach(value) {
+            resources.push({
+                "name"      : value,
+                "type"      : "audio",
+                "src"       : "data/sfx/",
+                "channel"   : 1
+            })
+        });
+
+        // Music.
+        this.resources["bgm"].forEach(function forEach(value) {
+            resources.push({
+                "name"      : value,
+                "type"      : "audio",
+                "src"       : "data/bgm/",
+                "channel"   : 2
+            })
+        });
+
+        // Load the resources.
+        me.loader.preload(resources);
+    },
+            
+            
     // Run on game resources loaded.
     "loaded": function() {
         me.state.set(me.state.MENU, new game.TitleScreen());
@@ -36,6 +114,7 @@ var game = {
         // add our player entity in the entity pool
         me.entityPool.add("mainPlayer", game.Player);
         me.entityPool.add("basicItem", game.basicItem);
+        me.entityPool.add("george",game.npc);
 
         // enable the keyboard
         me.input.bindKey(me.input.KEY.LEFT, "left");
